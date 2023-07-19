eb10f40cd3a5aa6abcf3e88163fdea2762a32328
Fabricio Lima <fabriciotrab2005@gmail.com>
2023-07-19 18:28:14 -0300

    feat: inicio da implementacao do tabuleiro

---
+++ xadrez-console/tabuleiro/Cor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro {
+    enum Cor {
+        BRANCO,
+        PRETO,
+        AMARELO,
+        AZUL,
+        VERMELHO,
+        VERDE,
+        LARANJA
+    }
+}
---
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace xadrez_console.tabuleiro {
+    internal class Tabuleiro {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] _pecas;
+    
+        public Tabuleiro(int linhas, int colunas) {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            _pecas = new Peca[linhas, colunas];
+        }
+    }
+}
