11b59ac95c2027d8a8ce0377e28c22889471304f
Fabricio Lima <fabriciotrab2005@gmail.com>
2023-07-19 19:31:26 -0300

    feat: melhorando a impressao do tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,9 +8,14 @@ namespace xadrez_console
     internal class Program {
         static void Main(string[] args) {
             try {
-                PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-                Console.WriteLine(pos.ToString());
-                Console.WriteLine(pos.toPosicao());
+                Tabuleiro tab = new Tabuleiro(8, 8);
+
+                tab.colocarPeca(new Torre(Cor.BRANCO, tab), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(Cor.PRETO, tab), new Posicao(0, 2));
+                tab.colocarPeca(new Torre(Cor.BRANCO, tab), new Posicao(0, 4));
+                tab.colocarPeca(new Rei(Cor.PRETO, tab), new Posicao(0, 5));
+
+                Tela.imprimirTabuleiro(tab);
             } catch (TabuleiroException ex) {
                 Console.WriteLine(ex.Message);
             }
--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -10,14 +10,29 @@ namespace xadrez_console {
     internal class Tela {
         public static void imprimirTabuleiro(Tabuleiro tab) {
             for (int i = 0; i < tab.linhas;  i++) {
+                Console.Write(8 - i + " ");
                 for(int j = 0; j < tab.colunas; j++) {
                     Posicao pos = new Posicao(i, j);
                     if(tab.peca(pos) == null) 
                         Console.Write("- ");
                     else 
-                        Console.Write($"{tab.peca(pos).ToString()} ");
+                        imprimirPeca(tab.peca(pos));
+                        Console.Write(" ");
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void imprimirPeca(Peca peca) {
+            if (peca.cor == Cor.BRANCO) {
+                Console.Write(peca);
+            }
+            else {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
